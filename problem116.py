129289501ac309f78b06b4191507b939c0588000
Pranav Mathur <pranavmathur99@gmail.com>
2016-02-27 23:40:52 -0800

    Solve 116

--- problem116.py
+++ problem116.py
@@ -1,5 +1,20 @@
-limit = 5
-reds = [0] * limit
-lastStart = 0
-for i in range(0, limit - 1):
-    reds[i] = 1 + reds[i-1]
+reds = [0, 0, 1]
+greens = [0, 0, 0, 1]
+blues = [0, 0, 0, 0, 1]
+for i in range(3, 51):
+    count = 0
+    for j in range(0, i-1):
+        count += (reds[j] + 1)
+    reds += [count]
+    
+for i in range(4, 51):
+    count = 0
+    for j in range(0, i-2):
+        count += (greens[j] + 1)
+    greens += [count]
+    
+for i in range(5, 51):
+    count = 0
+    for j in range(0, i-3):
+        count += (blues[j] + 1)
+    blues += [count]
